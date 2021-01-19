9a8a7cfd57b9b6607a734a5b9a7d97fc2d5f01b7
Mauricio Quaranta <mauricioqvp@yahoo.com.br>
2021-01-18 22:09:14 -0300

    first commit

---
+++ back-end/src/main/java/com/devsuperior/dsapostas/DsZupApostasApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsapostas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsZupApostasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsZupApostasApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/devsuperior/dsapostas/DsZupApostasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsapostas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsZupApostasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
